27d3e55442308a14a3075c0b31160509da0d56d1
CytahK <cytamakoni@gmail.com>
2024-10-31 23:39:12 +0200

    added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector(#temperature);
+    temperatureElement.innerHTML = response.data.temperature.current;
+    
+}
+
+
+function searchCity(city) {
+    let apiKey = "a4f302ccaf3b204cb26t8f848ba4659o";
+    let apiUrl =
+    `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);
+
+}
+
+
+
+
+function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
+}
+
+
+
+
 let searchFormElement = document.querySelector("#search-form");
-console.log(searchFormElement);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
